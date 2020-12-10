9c751fffa60154eb046c0df8e44606798e289d48
wahyuhadi <hrahmatwahyu@gmail.com>
2020-12-10 10:11:48 +0700

    add compton

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dwm
 - add in .xinitrc _JAVA_AWT_WM_NONREPARENTING=1
+```sh
+feh --bg-center /home/lemurian/Pictures/Wallpaper/mac.jpg &
+./.My-Config/My-DWM/dwm-bar/dwm_bar.sh &
+compton --config /home/lemurian/.config/compton/compton.conf &
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+amixer set Master unmute
+amixer sset 'Master' 100%
+exec dwm
+
+
+```
--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "fira code:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Terminus (TTF):pixelsize=17:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
